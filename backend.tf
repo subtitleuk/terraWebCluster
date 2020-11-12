5991d17e66d0823c6087b03c7a3c686e7fa090a2
Brendon Higgins <b_d_h@hotmail.com>
2020-11-12 20:17:34 +0000

    ASG validating with example code

--- backend.tf
+++ backend.tf
@@ -4,7 +4,7 @@ terraform {
     region  = "us-east-1"
     profile = "default"
     key     = "terraformstatefile"
-    bucket  = "terraformstatebucketbh11b1120"
+    bucket  = "terraformstatebucketbh121120"
   }
 }
 
--- services/asg/variables.tf
+++ services/asg/variables.tf
@@ -1,5 +1,6 @@
 variable "cluster_name" {
   description = "The name to use for all the cluster resources"
   type        = string
+  default     = "ha_cluster"
 }
 
